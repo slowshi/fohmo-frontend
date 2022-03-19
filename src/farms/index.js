f393784ef6ef5d2866763c118752c1c08e750cec
Len Lester <lentser921@gmail.com>
2022-03-19 13:23:02 -0700

    removed a lot of farms

--- src/farms/index.js
+++ src/farms/index.js
@@ -1,27 +1,27 @@
 // import ARB_FCS from './ARB-FCS.json';
 import ARB_UMAMI from './ARB-UMAMI.json';
-import ARB_OMIC from './ARB-OMIC.json';
-import AVAX_CAKE from './AVAX-CAKE.json';
-import AVAX_FORT from './AVAX-FORT.json';
+// import ARB_OMIC from './ARB-OMIC.json';
+// import AVAX_CAKE from './AVAX-CAKE.json';
+// import AVAX_FORT from './AVAX-FORT.json';
 import AVAX_CROWN from './AVAX-CROWN.json';
 // import AVAX_GG from './AVAX-GG.json';
-import AVAX_ICE from './AVAX-ICE.json';
-import AVAX_LF from './AVAX-LF.json';
+// import AVAX_ICE from './AVAX-ICE.json';
+// import AVAX_LF from './AVAX-LF.json';
 import AVAX_MAXI from './AVAX-MAXI.json';
-import AVAX_CLAVIS from './AVAX-CLAVIS.json';
-import AVAX_KARMA from './AVAX-KARMA.json'
+// import AVAX_CLAVIS from './AVAX-CLAVIS.json';
+// import AVAX_KARMA from './AVAX-KARMA.json'
 // import AVAX_NADO from './AVAX-NADO.json';
 import AVAX_OTWO from './AVAX-OTWO.json';
-import AVAX_PAPA from './AVAX-PAPA.json';
+// import AVAX_PAPA from './AVAX-PAPA.json';
 // import AVAX_PARR from './AVAX-PARR.json';
-import AVAX_RUG from './AVAX-RUG.json';
-import AVAX_ARA from './AVAX-ARA.json';
+// import AVAX_RUG from './AVAX-RUG.json';
+// import AVAX_ARA from './AVAX-ARA.json';
 // import AVAX_RGK from './AVAX-RGK.json';
 // import AVAX_GHOST from './AVAX-GHOST.json';
-import AVAX_SPACE from './AVAX-SPACE.json';
-import AVAX_KANDY from './AVAX-KANDY.json';
+// import AVAX_SPACE from './AVAX-SPACE.json';
+// import AVAX_KANDY from './AVAX-KANDY.json';
 // import AVAX_SPC from './AVAX-SPC.json';
-import AVAX_JOLLY from './AVAX-JOLLY.json';
+// import AVAX_JOLLY from './AVAX-JOLLY.json';
 // import AVAX_PEPE from './AVAX-PEPE.json'
 // import AVAX_GNIWS from './AVAX-GNIWS.json'
 // import AVAX_SPACEv1 from './AVAX-SPACEv1.json';
@@ -29,50 +29,50 @@ import AVAX_JOLLY from './AVAX-JOLLY.json';
 // import AVAX_SCAT from './AVAX-SCAT.json';
 import AVAX_PB from './AVAX-PB.json';
 // import AVAX_VERSE from './AVAX-VERSE.json'
-import AVAX_SB from './AVAX-SB.json';
-import AVAX_SEA from './AVAX-SEA.json';
-import AVAX_SRA from './AVAX-SRA.json';
+// import AVAX_SB from './AVAX-SB.json';
+// import AVAX_SEA from './AVAX-SEA.json';
+// import AVAX_SRA from './AVAX-SRA.json';
 // import AVAX_BLIGHT from './AVAX-BLIGHT.json';
 // import AVAX_BLANC from './AVAX-BLANC.json';
 import AVAX_TEMPO from './AVAX-TEMPO.json';
-import AVAX_VAL from './AVAX-VAL.json';
-import AVAX_VALDAO from './AVAX-VALDAO.json';
+// import AVAX_VAL from './AVAX-VAL.json';
+// import AVAX_VALDAO from './AVAX-VALDAO.json';
 import AVAX_TIME from './AVAX-TIME.json';
 import AVAX_MAG from './AVAX-MAG.json';
-import BSC_GYRO from './BSC-GYRO.json';
+// import BSC_GYRO from './BSC-GYRO.json';
 import BSC_XEUS from './BSC-XEUS.json';
-import BSC_PID from './BSC-PID.json';
+// import BSC_PID from './BSC-PID.json';
 import BSC_TRP from './BSC-TRP.json';
-import AVAX_CAT from './AVAX-CAT.json'
+// import AVAX_CAT from './AVAX-CAT.json'
 import CRO_MINO from './CRO-MINO.json'
 import BSC_GLBD from './BSC-GLBD.json'
-import BSC_ASGARD from './BSC-ASGARD.json';
+// import BSC_ASGARD from './BSC-ASGARD.json';
 // import BSC_SD from './BSC-SD.json';
-import BSC_JADE from './BSC-JADE.json';
+// import BSC_JADE from './BSC-JADE.json';
 import BSC_LOVE from './BSC-LOVE.json';
 import BSC_GG from './BSC-GG.json';
-import BSC_PAL from './BSC-PAL.json';
-import BSC_XPH from './BSC-XPH.json';
+// import BSC_PAL from './BSC-PAL.json';
+// import BSC_XPH from './BSC-XPH.json';
 import BSC_OX from './BSC-OX.json';
 import BSC_HXL from './BSC-HXL.json'
 import BSC_POWER from './BSC-POWER.json';
 // import BSC_RA from './BSC-RA.json';
-import BSC_META from './BSC-META.json';
+// import BSC_META from './BSC-META.json';
 import BSC_NMS from './BSC-NMS.json';
 // import BSC_TAC from './BSC-TAC.json';
 import BSC_TEM from './BSC-TEM.json';
-import BSC_HUMP from './BSC-HUMP.json';
+// import BSC_HUMP from './BSC-HUMP.json';
 import BSC_DIOS from './BSC-DIOS.json';
 import BSC_UDO from './BSC-UDO.json';
-import BSC_WHISKEY from './BSC-WHISKEY.json';
-import CRO_FORT from './CRO-FORT.json';
-import ETH_LOBI from './ETH-LOBI.json';
-import ETH_MNFST from './ETH-MNFST.json';
-import ETH_OHM from './ETH-OHM.json';
+// import BSC_WHISKEY from './BSC-WHISKEY.json';
+// import CRO_FORT from './CRO-FORT.json';
+// import ETH_LOBI from './ETH-LOBI.json';
+// import ETH_MNFST from './ETH-MNFST.json';
+// import ETH_OHM from './ETH-OHM.json';
 import ETH_OHM2 from './ETH-OHM2.json';
 import ETH_GAAS from './ETH-GAAS.json';
 import ETH_FLYZ from './ETH-FLYZ.json';
-import ETH_SQUID from './ETH-SQUID.json';
+// import ETH_SQUID from './ETH-SQUID.json';
 import ETH_BTRFLY from './ETH-BTRFLY.json';
 // import ETH_3DOG from './ETH-3DOG.json';
 // import FTM_GIZA from './FTM-GIZA.json';
@@ -82,8 +82,8 @@ import FTM_HEC from './FTM-HEC.json';
 import FTM_LUX from './FTM-LUX.json';
 import FTM_SPA from './FTM-SPA.json';
 import FTM_SCR from './FTM-SCR.json';
-import FTM_WEN from './FTM-WEN.json';
-import FTM_CYBER from './FTM-CYBER.json';
+// import FTM_WEN from './FTM-WEN.json';
+// import FTM_CYBER from './FTM-CYBER.json';
 // import FTM_VOLT from './FTM-VOLT.json';
 // import FTM_PUMP from './FTM-PUMP.json';
 import KLAY_KRNO from './KLAY-KRNO.json';
@@ -92,7 +92,7 @@ import MATIC_CLAM2 from './MATIC-CLAM2.json';
 import MATIC_VSQ from './MATIC-VSQ.json';
 import MATIC_GURU from './MATIC-GURU.json';
 import MATIC_KLIMA from './MATIC-KLIMA.json';
-import MATIC_MAMA from './MATIC-MAMA.json';
+// import MATIC_MAMA from './MATIC-MAMA.json';
 import MATIC_PRXY from './MATIC-PRXY.json';
 import MOVR_FHM from './MOVR-FHM.json';
 import MOVR_ROME from './MOVR-ROME.json'
@@ -108,81 +108,81 @@ import ONE_TEM from './ONE-TEM.json';
 const farms = {
   // ...ARB_Z20,
   // ...ARB_FCS,
-  ...ARB_OMIC,
+  // ...ARB_OMIC,
   ...ARB_UMAMI,
-  ...AVAX_CAKE,
-  ...AVAX_FORT,
+  // ...AVAX_CAKE,
+  // ...AVAX_FORT,
   // ...AVAX_GG,
-  ...AVAX_ICE,
-  ...AVAX_LF,
+  // ...AVAX_ICE,
+  // ...AVAX_LF,
   ...AVAX_MAXI,
   ...AVAX_CROWN,
   // ...AVAX_PEPE,
-  ...AVAX_CLAVIS,
+  // ...AVAX_CLAVIS,
   // ...AVAX_NADO,
   ...AVAX_OTWO,
-  ...AVAX_PAPA,
-  ...AVAX_ARA,
-  ...AVAX_KARMA,
-  ...AVAX_RUG,
+  // ...AVAX_PAPA,
+  // ...AVAX_ARA,
+  // ...AVAX_KARMA,
+  // ...AVAX_RUG,
   ...AVAX_MAG,
-  ...AVAX_KANDY,
+  // ...AVAX_KANDY,
   // ...AVAX_RGK,
   // ...AVAX_GNIWS,
   // ...AVAX_PARR,
   // ...AVAX_BLANC,
   // ...AVAX_SCAT,
   ...AVAX_PB,
-  ...AVAX_SEA,
-  ...AVAX_SRA,
-  ...AVAX_CAT,
+  // ...AVAX_SEA,
+  // ...AVAX_SRA,
+  // ...AVAX_CAT,
   // ...AVAX_VERSE,
-  ...AVAX_SB,
+  // ...AVAX_SB,
   // ...AVAX_SBR,
-  ...AVAX_SPACE,
+  // ...AVAX_SPACE,
   // ...AVAX_GHOST,
   // ...AVAX_SPACEv1,
   // ...AVAX_BLIGHT,
   ...AVAX_TEMPO,
   ...AVAX_TIME,
-  ...AVAX_VAL,
+  // ...AVAX_VAL,
   // ...AVAX_SPC,
-  ...AVAX_JOLLY,
-  ...AVAX_VALDAO,
-  ...CRO_FORT,
+  // ...AVAX_JOLLY,
+  // ...AVAX_VALDAO,
+  // ...CRO_FORT,
   ...CRO_MINO,
   ...BSC_DIOS,
-  ...BSC_GYRO,
+  // ...BSC_GYRO,
   ...BSC_XEUS,
-  ...BSC_JADE,
-  ...BSC_HUMP,
+  // ...BSC_JADE,
+  // ...BSC_HUMP,
   ...BSC_GLBD,
-  ...BSC_META,
+  // ...BSC_META,
   ...BSC_GG,
   ...BSC_LOVE,
   ...BSC_HXL,
   ...BSC_POWER,
-  ...BSC_PAL,
+  // ...BSC_PAL,
   ...BSC_NMS,
-  ...BSC_ASGARD,
+  // ...BSC_ASGARD,
   ...BSC_OX,
   // ...BSC_SD,
-  ...BSC_PID,
+  // ...BSC_PID,
   // ...BSC_RA,
   // ...BSC_TAC,
   ...BSC_TEM,
   ...BSC_UDO,
   ...BSC_TRP,
-  ...BSC_XPH,
-  ...BSC_WHISKEY,
-  ...ETH_LOBI,
-  ...ETH_MNFST,
-  ...ETH_OHM,
+  // ...BSC_XPH,
+  // ...BSC_WHISKEY,
+  // ...ETH_LOBI,
+  // ...ETH_MNFST,
+  // ...ETH_OHM,
   ...ETH_OHM2,
   ...ETH_GAAS,
   // ...ETH_3DOG,
   ...ETH_BTRFLY,
-  ...ETH_SQUID,
+  // ...ETH_SQUID,
   ...ETH_FLYZ,
   ...FTM_EXOD,
   // ...FTM_VOLT,
@@ -193,14 +193,14 @@ const farms = {
   ...FTM_LUX,
   ...FTM_SPA,
   // ...FTM_PUMP,
-  ...FTM_WEN,
-  ...FTM_CYBER,
+  // ...FTM_WEN,
+  // ...FTM_CYBER,
   ...KLAY_KRNO,
   // ...MATIC_CLAM,
   ...MATIC_CLAM2,
   ...MATIC_GURU,
   ...MATIC_VSQ,
-  ...MATIC_MAMA,
+  // ...MATIC_MAMA,
   ...MATIC_KLIMA,
   ...MATIC_PRXY,
   ...MOVR_FHM,
